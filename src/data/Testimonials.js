e5f7b1bb501eec8da077c51d0e7bf5a2fa975e33
Hamim Reja <hamim.reja.mail@gmail.com>
2025-08-15 17:36:18 +0530

    Review

--- src/data/Testimonials.js
+++ src/data/Testimonials.js
@@ -5,4 +5,10 @@ export const testimonialsList = [
     star: "5",
     text: "Working with Hamim was an amazing experience. He understood exactly what I wanted and turned my ideas into a modern, responsive, and professional website. The process was smooth, and he delivered earlier than promised. Highly recommended!",
   },
+  {
+    name: "Apurba Maitra",
+    role: "Assistant Teacher(Madhaipur A.R. High School(H.S)",
+    star: "5",
+    text: "The website design has been made very carefully and skillfully.",
+  },
 ];
